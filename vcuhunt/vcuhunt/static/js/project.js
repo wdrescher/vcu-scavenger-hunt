d6b39bc086a55f84ee86dc24370e48f0aca14a87
Bryan Hilldrup <hilldrupbf@vcu.edu>
2020-02-16 21:54:03 -0500

    Basic landmark page working

--- vcuhunt/vcuhunt/static/js/project.js
+++ vcuhunt/vcuhunt/static/js/project.js
@@ -1,1 +1,80 @@
 /* Project specific Javascript goes here. */
+
+
+<script>
+    function openForm() {
+        document.getElementById("quiz").style.display = "block";
+    }
+    
+    function closeForm() {
+        document.getElementById("quiz").style.display = "none";
+    }
+
+    function submitAnswer() {
+    var radios = document.getElementsByName("Quiz Question");
+    var i = 0, len = radios.length;
+    var checked = false;
+    var userAnswer;
+    
+    for( ; i < len; i++ ) {
+        if(radios[i].checked) {
+        checked = true;
+        userAnswer = radios[i].value;
+        }
+    } 
+    // if user click submit button without selecting any option, alert box should be say "please select choice answer".
+    if(!checked) {
+        alert("Please select an answer");
+        return;
+    }
+    // Correct answer
+    if(userAnswer === "maison") {
+        alert("Answer is correct!");
+    }
+    // incorrect answer
+    else {
+        alert("Answer is wrong!");
+    }
+    
+    }
+
+</script>
+{% endblock quizScript %}
+<script>
+    function openForm() {
+        document.getElementById("quiz").style.display = "block";
+    }
+    
+    function closeForm() {
+        document.getElementById("quiz").style.display = "none";
+    }
+
+    function submitAnswer() {
+    var radios = document.getElementsByName("Quiz Question");
+    var i = 0, len = radios.length;
+    var checked = false;
+    var userAnswer;
+    
+    for( ; i < len; i++ ) {
+        if(radios[i].checked) {
+        checked = true;
+        userAnswer = radios[i].value;
+        }
+    } 
+    // if user click submit button without selecting any option, alert box should be say "please select choice answer".
+    if(!checked) {
+        alert("Please select an answer");
+        return;
+    }
+    // Correct answer
+    if(userAnswer === "maison") {
+        alert("Answer is correct!");
+    }
+    // incorrect answer
+    else {
+        alert("Answer is wrong!");
+    }
+    
+    }
+
+</script>
