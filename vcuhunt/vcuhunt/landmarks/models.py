811e5de7725658dd7df758fad4cccd486361efbf
willem <willemdrescher@yahoo.com>
2020-02-23 21:57:03 -0500

    add landmark images

--- vcuhunt/vcuhunt/landmarks/models.py
+++ vcuhunt/vcuhunt/landmarks/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Landmark(models.Model): 
     name = models.CharField(blank=True, max_length=255)
+    image_url = models.CharField(blank=True, max_length=500)
 
     def get_absolute_url(self):
         return reverse("landmarks:detail", kwargs={"id": self.id})
+    
+    def __str__(self): 
+        return self.name; 
