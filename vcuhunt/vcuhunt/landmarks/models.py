2688ef575a29cb8d17d0def0be084d891d58b0df
anster98 <43069641+anster98@users.noreply.github.com>
2020-02-26 14:54:59 -0500

    Made detail page and quiz dynamic

--- vcuhunt/vcuhunt/landmarks/models.py
+++ vcuhunt/vcuhunt/landmarks/models.py
@@ -5,6 +5,15 @@ from django.urls import reverse
 class Landmark(models.Model): 
     name = models.CharField(blank=True, max_length=255)
     image_url = models.CharField(blank=True, max_length=500)
+    address = models.CharField(blank=True, max_length=500)
+    description = models.CharField(blank=True, max_length=500)
+    question = models.CharField(blank=True, max_length=500)
+    c_answer = models.CharField(blank=True, max_length=500)
+    answer1 = models.CharField(blank=True, max_length=500)
+    answer2 = models.CharField(blank=True, max_length=500)
+    answer3 = models.CharField(blank=True, max_length=500)
+    answer4 = models.CharField(blank=True, max_length=500)
+    map_query = models.CharField(blank=True, max_length=500)
 
     def get_absolute_url(self):
         return reverse("landmarks:detail", kwargs={"id": self.id})
