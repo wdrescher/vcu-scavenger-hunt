18253f7b3f2ef147bd5b572dd501b3d2cce07cc2
Willem Drescher <willemdrescher@Willems-MBP.fios-router.home>
2020-04-01 16:21:36 -0400

    add fix for un logged in users

--- vcuhunt/landmarks/views.py
+++ vcuhunt/landmarks/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from vcuhunt.landmarks.models import Landmark
-
 # Create your views here.
 class LandmarkCompletedListView(ListView):
     model = Landmark
 
     def get_queryset(self):
-        return self.request.user.completed_landmark.all()
+        if (self.request.user): 
+            return self.request.user.completed_landmark.all()
+        return Landmark.objects.all()
 
 class LandmarkUnCompletedListView(ListView):
     model = Landmark
 
     def get_queryset(self):
-        landmarks_list = Landmark.objects.all()
-        completed = self.request.user.completed_landmark.all()
-        todo = set()
-        for landmark in landmarks_list: 
-            if landmark not in completed: 
-                todo.add(landmark.id)
-        return Landmark.objects.filter(pk__in = todo)
+        if (self.request.user.is_authenticated): 
+            landmarks_list = Landmark.objects.all()
+            completed = self.request.user.completed_landmark.all()
+            todo = set()
+            for landmark in landmarks_list: 
+                if landmark not in completed: 
+                    todo.add(landmark.id)
+            return Landmark.objects.filter(pk__in = todo)
+        return Landmark.objects.all()
     
  
-class LandmarkDetailView(DetailView):
+class LandmarkDetailView(LoginRequiredMixin, DetailView):
     model = Landmark 
